91cbbe5320a6891ba2e96fdf0cef220bf6b7789e
thien <thiendam052@gmail.com>
2023-12-04 09:50:48 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,131 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, {useState} from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+
+import {
+  Colors,
+  // DebugInstructions,
+  Header,
+  // LearnMoreLinks,
+  // ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function Section({children, title}: SectionProps): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(prevCount => prevCount + 1);
+  const onReset = () => setCount(0);
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Hello guys <Text style={styles.highlight}>welcome</Text>
+          </Section>
+          <View>
+            <Text>Count: {count}</Text>
+          </View>
+          <TouchableOpacity style={styles.button} onPress={onPress}>
+            <Text style={styles.buttonTitle}>Press Here</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={onReset}>
+            <Text style={styles.buttonTitle}>Reset</Text>
+          </TouchableOpacity>
+          {/* <LearnMoreLinks /> */}
+          <Text>Thien Dam Dep trai</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#aaaaaa',
+    padding: 10,
+    margin: 5,
+  },
+  buttonTitle: {
+    color: 'black',
+    fontSize: 15,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
